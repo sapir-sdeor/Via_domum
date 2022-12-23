865313ff9a489d89382008a1b1c6e68551cd2890
nitzankur <nitzan.knoll@mail.huji.ac.il>
2022-12-23 11:51:26 +0200

    with oil lamp
    need to do - גרזן, לתקן תאורה, לתקן את החיבור לעששית

---
+++ Assets/Scripts/OilLamp.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OilLamp : MonoBehaviour
+{
+    [SerializeField] private Vector3 newScale;
+    [SerializeField] private float lampPos = 0.1f;
+    [SerializeField] private int playerNumber;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (playerNumber == 1 && other.gameObject.name == UIManager.PLAYER1||
+            playerNumber == 2 && other.gameObject.name == UIManager.PLAYER2)
+        {
+            var trans = other.gameObject.transform;
+            var pos = trans.position;
+            GameObject oilLamp = Instantiate(gameObject,pos+Vector3.left*lampPos,
+                Quaternion.identity,trans.transform); 
+            oilLamp.GetComponent<Collider2D>().enabled = false;
+            oilLamp.transform.localScale = newScale;
+            Destroy(gameObject);
+        }
+    }
+}
