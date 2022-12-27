ab1c2b5cffc1347e5e5727f12dc8aa7f0137de83
nitzankur <nitzan.knoll@mail.huji.ac.il>
2022-12-27 08:00:46 +0200

    with start of menu

--- Assets/Scripts/Acting.cs
+++ Assets/Scripts/Acting.cs
@@ -117,11 +117,14 @@ public class Acting : MonoBehaviour
         if (gameObject.name == UIManager.PLAYER1)
         {
             if (!uiManager || !uiManager.getUIOpen1()) SetMoveAnimation(context);
+            else  uiManager.NavigateMenu1(context);
         }
 
         if (gameObject.name == UIManager.PLAYER2)
         {
             if (!uiManager || !uiManager.getUIOpen2()) SetMoveAnimation(context);
+            else uiManager.NavigateMenu2(context);
+
         }
     }
 
