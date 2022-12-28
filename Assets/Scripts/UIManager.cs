cf226782e0171289ec9dd6c04cf59f1752de5f7a
nitzankur <nitzan.knoll@mail.huji.ac.il>
2022-12-29 00:20:02 +0200

    complete ui

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -12,13 +12,13 @@ public class UIManager : MonoBehaviour
     private static bool _useFirstPower1 = true;
     private static bool _useFirstPower2 = true;
     private static int _powerCounterPlayer1 = -1;
-    private static  int _powerCounterPlayer2;
+    private static  int _powerCounterPlayer2=-1;
     public static string PLAYER1 = "Player1";
     public static string PLAYER2 = "Player2";
     private static bool _uiOpen1, _uiOpen2;
     
-    private int _indexPowerPlayer1, _indexPowerPlayer2;
-    private int _indexHor1, _indexVer1,_indexHor2,_indexVer2;
+    private int _indexPowerPlayer1=0, _indexPowerPlayer2;
+    private int _indexHor1,_indexHor2;
     private LevelManager _levelManager;
     private bool _flyAlready;
     private Button[] buttonManager1, buttonManager2;
@@ -28,6 +28,7 @@ public class UIManager : MonoBehaviour
 
     private PlayerMovement.UIActions UImanager;
     private static readonly int Collect = Animator.StringToHash("collect");
+    [SerializeField] private Sprite[] _spriteGlow,_sprites;
 
     private void Start()
     {
@@ -43,12 +44,21 @@ public class UIManager : MonoBehaviour
 
     public void CancelPlayer1(InputAction.CallbackContext context)
     {
+        print("cancel player" + _indexPowerPlayer1);
         _uiOpen1 = !_uiOpen1;
         if (_uiOpen1)
         {
             _indexHor1 = 0;
-            _indexVer1 = 0;
-            buttonManager1[_indexHor1].image.color = Color.magenta;
+         
+            for (int j = 0; j <_spriteGlow.Length; j++)
+            {
+                String spriteName = _spriteGlow[j].name;
+                if (buttonManager1[_indexPowerPlayer1].CompareTag(spriteName))
+                {
+                    buttonManager1[_indexPowerPlayer1].GetComponent<Image>().sprite = _spriteGlow[j]; 
+                    break;
+                }
+            }
         }
         
         SetActiveUIobject(buttonManager1,_uiOpen1);
@@ -66,7 +76,16 @@ public class UIManager : MonoBehaviour
         if (_uiOpen2)
         {
             _indexHor2 = 0;
-            _indexVer2 = 0;
+         
+            for (int j = 0; j <_spriteGlow.Length; j++)
+            {
+                String spriteName = _spriteGlow[j].name;
+                if (buttonManager2[_indexPowerPlayer2].CompareTag(spriteName))
+                {
+                    buttonManager2[_indexPowerPlayer2].GetComponent<Image>().sprite = _spriteGlow[j]; 
+                    break;
+                }
+            }
         }
         SetActiveUIobject(buttonManager2,_uiOpen2);
         _levelManager.CloseUIMessage(PLAYER2);
@@ -88,6 +107,7 @@ public class UIManager : MonoBehaviour
     
     public void ApplyPowerPlayer1(InputAction.CallbackContext context)
     {
+        print(_indexPowerPlayer1 + " apply player");
         if (context.performed)
         {
             if (_powerCounterPlayer1 < 0)
@@ -113,6 +133,7 @@ public class UIManager : MonoBehaviour
     
     public void ApplyPowerPlayer2(InputAction.CallbackContext context)
     {
+        print(_indexPowerPlayer2 + " apply player");
         if (context.performed)
         {
             if (_powerCounterPlayer2 < 0)
@@ -151,43 +172,7 @@ public class UIManager : MonoBehaviour
     {
         return _uiOpen2;
     }
-
-    private void Update()
-    {
-        // if (LevelManager.GETLevel() == 1&& setFalse1)
-        // {
-        //     var gameObjects = GameObject.FindGameObjectsWithTag("little ui");
-        //     foreach (var newPower in gameObjects)
-        //     {
-        //         newPower.gameObject.SetActive(false);
-        //     } 
-        //     power1 = gameObjects;
-        //     setFalse1 = false;
-        // }
-        // else if (LevelManager.GETLevel() == 2 && setFalse2)
-        // {
-        //     print("level 2 should work");
-        //     var gameObjectsFly = GameObject.FindGameObjectsWithTag("fly ui");
-        //     var gameObjectsBlow = GameObject.FindGameObjectsWithTag("blowUp ui");
-        //     print(gameObjectsBlow.Length +" count blow");
-        //     print(gameObjectsFly.Length +"count fly");
-        //     
-        //     foreach (var newPower in gameObjectsFly)
-        //     {
-        //         print(newPower.gameObject.name + " name fly");
-        //         newPower.gameObject.SetActive(false);
-        //     }
-        //
-        //     foreach (var newPower in gameObjectsBlow)
-        //     {
-        //         print(newPower.gameObject.name + " name blow up");
-        //         newPower.gameObject.SetActive(false);
-        //     } 
-        //     power2 = gameObjectsFly;
-        //     power3 = gameObjectsBlow;
-        //     setFalse2 = false;
-        // }
-    }
+    
 
 
     //when one of the players collect power - update the counter and make the power available
@@ -195,25 +180,45 @@ public class UIManager : MonoBehaviour
     {
         if (player.name== PLAYER1)
         {
-            buttonManager1[_indexPowerPlayer1].gameObject.tag = power.gameObject.tag;
             if (_openFirstUI1)
             {
                 _levelManager.OpenUIMessage(player);
                 _openFirstUI1 = false;
             }
             _powerCounterPlayer1++;
+            buttonManager1[_powerCounterPlayer1].gameObject.tag = power.gameObject.tag;
+            for (int j = 0; j <_sprites.Length; j++)
+            {
+                String spriteName = _sprites[j].name;
+                if (buttonManager1[_powerCounterPlayer1].CompareTag(spriteName))
+                {
+                    buttonManager1[_powerCounterPlayer1].GetComponent<Image>().sprite = _sprites[j];
+                    buttonManager1[_powerCounterPlayer1].GetComponent<Image>().color = Color.white;
+                    break;
+                }
+            }
             buttonManager1[_powerCounterPlayer1].interactable = true;
             ShowNewPower(power.transform);
         }
         else if (player.name == PLAYER2)
         {
-            buttonManager2[_indexPowerPlayer2].gameObject.tag = power.gameObject.tag;
             if (_openFirstUI2)
             {
                 _levelManager.OpenUIMessage(player);
                 _openFirstUI2 = false;
             }
             _powerCounterPlayer2++;
+            buttonManager2[_powerCounterPlayer2].gameObject.tag = power.gameObject.tag;
+            for (int j = 0; j <_sprites.Length; j++)
+            {
+                String spriteName = _sprites[j].name;
+                if (buttonManager2[_powerCounterPlayer2].CompareTag(spriteName))
+                {
+                    buttonManager2[_powerCounterPlayer2].GetComponent<Image>().sprite = _sprites[j];
+                    buttonManager2[_powerCounterPlayer2].GetComponent<Image>().color = Color.white;
+                    break;
+                }
+            }
             buttonManager2[_powerCounterPlayer2].interactable = true;
             ShowNewPower(power.transform);
         }
@@ -221,19 +226,74 @@ public class UIManager : MonoBehaviour
 
     public void NavigateMenu1(InputAction.CallbackContext context)
     {
-        print(context.ReadValue<Vector2>().x);
-        print(_indexHor1+" hor "+ _powerCounterPlayer1+" player");
+        print(_indexHor1 +" index hor");
         if (context.ReadValue<Vector2>().x > 0 && _indexHor1 < _powerCounterPlayer1) _indexHor1++;
         else if (context.ReadValue<Vector2>().x < 0 && _indexHor1 >= 1) _indexHor1--;
         for (int i = 0; i <= _powerCounterPlayer1; i++)
         {
-            if(i!= _indexHor1) buttonManager1[i].image.color = Color.white;
-            else{buttonManager1[_indexHor1].image.color = Color.magenta;}
+            if (i != _indexHor1)
+            {
+                for (int j = 0; j <_sprites.Length; j++)
+                {
+                    String spriteName = _sprites[j].name;
+                    if (buttonManager1[_indexHor1].CompareTag(spriteName))
+                    {
+                        buttonManager1[_indexHor1].GetComponent<Image>().sprite = _sprites[j]; 
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for (int j = 0; j <_spriteGlow.Length; j++)
+                {
+                    String spriteName = _spriteGlow[j].name;
+                    if (buttonManager1[_indexHor1].CompareTag(spriteName))
+                    {
+                        buttonManager1[_indexHor1].GetComponent<Image>().sprite = _spriteGlow[j]; 
+                        break;
+                    }
+                }
+            }
         }
+
+        _indexPowerPlayer1 = _indexHor1;
     }
 
     public void NavigateMenu2(InputAction.CallbackContext context)
     {
+        print(_indexHor2 +" index hor");
+        if (context.ReadValue<Vector2>().x > 0 && _indexHor2 < _powerCounterPlayer2) _indexHor2++;
+        else if (context.ReadValue<Vector2>().x < 0 && _indexHor2 >= 1) _indexHor2--;
+        for (int i = 0; i <= _powerCounterPlayer2; i++)
+        {
+            if (i != _indexHor2)
+            {
+                for (int j = 0; j <_sprites.Length; j++)
+                {
+                    String spriteName = _sprites[j].name;
+                    if (buttonManager2[_indexHor2].CompareTag(spriteName))
+                    {
+                        buttonManager2[_indexHor2].GetComponent<Image>().sprite = _sprites[j]; 
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for (int j = 0; j <_spriteGlow.Length; j++)
+                {
+                    String spriteName = _spriteGlow[j].name;
+                    if (buttonManager2[_indexHor2].CompareTag(spriteName))
+                    {
+                        buttonManager2[_indexHor2].GetComponent<Image>().sprite = _spriteGlow[j]; 
+                        break;
+                    }
+                }
+            }
+        }
+
+        _indexPowerPlayer2 = _indexHor2;
         
     }
 
@@ -241,27 +301,9 @@ public class UIManager : MonoBehaviour
     {
         foreach (Transform newPowerChild in power)
         {
-            print(newPowerChild.gameObject +" new power tag");
             newPowerChild.gameObject.SetActive(true);
         }
         StartCoroutine(SetOffMessage(power));
-        
-        // GameObject[] power;
-        // switch (powerName)
-        // {
-        //     case "little":
-        //         power = power1;
-        //         break;
-        //     case "fly":
-        //         power = power2;
-        //         break;
-        //     case "blowUp":
-        //         power = power3;
-        //         break;
-        //     default:
-        //         power = null;
-        //         break;
-        // }
     }
     static IEnumerator SetOffMessage(Transform gameObjects)
     {
