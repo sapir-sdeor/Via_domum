861659c50f454882e2032d421d4c98c765cbbb44
sapirsdeor <105819753+sapir-sdeor@users.noreply.github.com>
2023-01-18 16:05:29 +0200

    Add ehco

---
+++ Assets/Scripts/CoreMechanic/Echo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace CoreMechanic
+{
+    public class Echo : MonoBehaviour, ICoreMechanic
+    {
+        private static readonly int ApplyEcho = Animator.StringToHash("applyEcho");
+
+        public void ApplyMechanic()
+        {
+            GetComponent<Animator>().SetTrigger("echo");
+            Collider2D collider = Physics2D.OverlapCircle
+                (transform.position, 0.2f, LayerMask.NameToLayer("Echo"));
+            if (collider)
+            {
+                collider.gameObject.GetComponent<Animator>().SetTrigger(ApplyEcho);
+            }
+        }
+
+    }
+}
