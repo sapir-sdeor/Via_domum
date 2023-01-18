e98ab7cd38ee77166fa061e1b5f518494424825e
sapirsdeor <105819753+sapir-sdeor@users.noreply.github.com>
2023-01-18 16:18:24 +0200

    flower

--- Assets/Scripts/CoreMechanic/Fly.cs
+++ Assets/Scripts/CoreMechanic/Fly.cs
@@ -6,21 +6,16 @@ namespace CoreMechanic
 {
     public class Fly : MonoBehaviour, ICoreMechanic
     {
-        private float _time;
         private bool _fly;
         private Vector3 _flyPos;
         private float speed = 1.5f;
-        private GameObject[] _particle;
         private GameObject[] _flower;
         private int _indexFlower;
-        private bool _explodeFlower;
         private static readonly int Explode = Animator.StringToHash("explode");
         private static readonly int Fly1 = Animator.StringToHash("fly");
-        private bool _applyPower;
 
         private void Awake()
         {
-            _particle = GameObject.FindGameObjectsWithTag("particle system");
             _flower = GameObject.FindGameObjectsWithTag("flower");
         }
 
@@ -31,17 +26,6 @@ namespace CoreMechanic
         
         private void Update()
         {
-            _time += Time.deltaTime;
-            if (_time > 4)
-            {
-                _time = 0;
-                if (_indexFlower < _particle.Length && _applyPower)
-                {
-                    for (int i = 0; i <= _indexFlower; i++)
-                        _particle[i].GetComponent<ParticleSystem>().Stop();
-                    _applyPower = false;
-                }
-            }
             if (Vector3.Distance(transform.position, _flyPos) < 0.3f)
             {
                 _fly = false;
@@ -60,27 +44,13 @@ namespace CoreMechanic
 
         public void ApplyMechanic()
         {
-            if (_indexFlower < _particle.Length) 
-                _particle[_indexFlower].GetComponent<ParticleSystem>().Play();
             if (_indexFlower < _flower.Length)
             {
                 _flower[_indexFlower].GetComponent<Animator>().SetBool(Explode, true);
                 if (_flower[_indexFlower].name == "Main Flower")
                     StartFlying();
             }
-            _applyPower = true;
             _indexFlower++;
-        }
-
-        IEnumerator StartAnimation()
-        {
-            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            yield return new WaitForSeconds(2);
-            _fly = true;
-            GetComponent<Rigidbody2D>().gravityScale = 0;
-            GetComponent<SpriteRenderer>().maskInteraction = SpriteMaskInteraction.None;
-            GetComponent<Collider2D>().enabled = false;
-            GetComponent<Animator>().SetBool(Fly1, true);
         }
 
         private void StartFlying()
--- Assets/Scripts/mainFlower.cs
+++ Assets/Scripts/mainFlower.cs
@@ -1,9 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class mainFlower : MonoBehaviour
 {
+    private void Start()
+    {
+        PlayFlowersAnimation();
+    }
+
+    private void PlayFlowersAnimation()
+    {
+        foreach (var animator in GetComponentsInChildren<Animator>())
+        {
+            animator.SetTrigger("wind");
+        }
+    }
     
     
     
