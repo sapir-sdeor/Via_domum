b63113aaa87fe586e20fc8a1f92d44b3258f9ec7
nitzankur <nitzan.knoll@mail.huji.ac.il>
2023-01-18 12:58:17 +0200

    with fixed animation

--- Assets/Scripts/NewPowerAnimation.cs
+++ Assets/Scripts/NewPowerAnimation.cs
@@ -1,17 +1,19 @@
-using System;
+
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class NewPowerAnimation : MonoBehaviour
 {
     [SerializeField] private Animator newPowerAnimator;
+    [SerializeField] private GameObject aura, text;
+    [SerializeField] private float TimeToWait=0.7f;
     private static readonly int Player1 = Animator.StringToHash("player1");
     private static readonly int Player2 = Animator.StringToHash("player2");
 
 
     private void OnCollisionEnter2D(Collision2D other)
     {
+        StartCoroutine(waitBeforeDestroy());
         if (other.gameObject.name == UIManager.PLAYER1)
         {
             newPowerAnimator.SetBool(Player1, true);
@@ -20,5 +22,12 @@ public class NewPowerAnimation : MonoBehaviour
         {
             newPowerAnimator.SetBool(Player2, true);
         }
+    }
+
+    IEnumerator waitBeforeDestroy()
+    {
+        yield return new WaitForSeconds(TimeToWait);
+        aura.SetActive(false);
+        text.SetActive(false);
     }
 }
