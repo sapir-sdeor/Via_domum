91f44016d46765af9ce2bf08594cb823ee6f915b
nitzankur <nitzan.knoll@mail.huji.ac.il>
2023-01-18 15:59:10 +0200

    with ui animation

--- Assets/Scripts/NewPowerAnimation.cs
+++ Assets/Scripts/NewPowerAnimation.cs
@@ -1,38 +1,55 @@
 
+using System;
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewPowerAnimation : MonoBehaviour
 {
     [SerializeField] private Animator newPowerAnimator;
+    private Animator auraAnimator;
     [SerializeField] private GameObject aura, text;
     [SerializeField] private float TimeToWait=0.7f;
     private static readonly int Player1 = Animator.StringToHash("player1");
     private static readonly int Player2 = Animator.StringToHash("player2");
+    private static readonly int FadeIn = Animator.StringToHash("fadeIn");
+    private static readonly int FadeOut = Animator.StringToHash("fadeOut");
+
 
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        StartCoroutine(waitBeforeDestroy(other));
-      
-       
+        newPowerAnimator.gameObject.SetActive(true);
+        auraAnimator = aura.GetComponent<Animator>();
+        auraAnimator.gameObject.SetActive(true);
+        auraAnimator.SetTrigger(FadeIn);
+        if (other.gameObject.name == UIManager.PLAYER1)
+        {
+            StartCoroutine(waitBeforeDestroy1());
+        }
+        else if (other.gameObject.name == UIManager.PLAYER2)
+        {
+            StartCoroutine(waitBeforeDestroy2());
+            auraAnimator.SetBool(FadeIn,false);
+        }
     }
+    
 
-    IEnumerator waitBeforeDestroy(Collision2D other)
+    IEnumerator waitBeforeDestroy1()
     {
         yield return new WaitForSeconds(TimeToWait);
         aura.SetActive(false);
         text.SetActive(false);
-        if (other.gameObject.name == UIManager.PLAYER1)
-        {
-            print("animation1");
-            newPowerAnimator.SetBool(Player1, true);
-        }
-        else if (other.gameObject.name == UIManager.PLAYER2)
-        {
-            print("animation2");
-            newPowerAnimator.SetBool(Player2, true);
-        }
-        
+        auraAnimator.SetBool(FadeOut, true);
+        newPowerAnimator.SetBool(Player1, true);
+    }
+    
+    IEnumerator waitBeforeDestroy2()
+    {
+        yield return new WaitForSeconds(TimeToWait);
+        aura.SetActive(false);
+        text.SetActive(false);
+        auraAnimator.SetBool(FadeOut,true);
+        newPowerAnimator.SetBool(Player2, true);
     }
 }
